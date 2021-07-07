28028f92b580816f478889d2e4b15a994fb75cbf
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-07 11:48:36 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const nanoId = require("nanoId");
+const nanoId = require("nanoid");
+var app = express()
+
+app.use(cors())
+
+app.post('http:localhost:8000/', function() {
+    console.log("App is listening on port 8000")
+})
