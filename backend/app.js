dcb9639b64fb12dabd7f7e746eea23406c460271
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-07 11:51:43 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,9 @@ var app = express()
 app.use(cors())
 
 app.post('http://localhost:8000/', function() {
-    console.log("App is listening on port 8000")
+    
 })
+
+app.listen(8000, function () {
+    console.log('CORS-enabled web server listening on port 8000')
+  })
