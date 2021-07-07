235ff7021e03fc2590909feecf6fea71e0128e02
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-07 11:22:46 +0800

    Update index.js

--- website/index.js
+++ website/index.js
@@ -2,8 +2,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     window.alert('DOM fully loaded and parsed');
 });
 
-var createSessionButton = document.getElementById('create-session-button');
+var createSessionButton = document.getElementById("create-session-button");
 
-document.getElementById('create-session-button').addEventListener("click", (event) => {
+document.getElementById("create-session-button").addEventListener("click", (event) => {
     window.alert('Button pressed');
 });
