11ae314042b142a805c95eaf6047d02cdaec5427
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-08 14:36:33 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const {nanoid} = require("nanoid");
 var app = express()
 app.use(cors())
 
+sessionId = nanoid(10);
+var sessions = {
+    sessionId: 1
+}
+
 app.post('/', function(req, res) {
     res.send(sessions)
 })
@@ -13,7 +18,3 @@ app.listen(8000, function () {
     console.log('CORS-enabled web server listening on port 8000');
 });
 
-sessionId = nanoid(10);
-var sessions = {
-    sessionId: 1
-}
