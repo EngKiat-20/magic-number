369f1d41987582a4ad707efe1ed90c4861d909f3
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-15 14:54:11 +0800

    Update index.js

--- website/index.js
+++ website/index.js
@@ -30,15 +30,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 return response.json();
             })
             .then(function (json) {
-                // Extract the lower and upper bound
+                
                 const lowerBound = json[0];
                 const upperBound = json[1];
 
-                // Get the reference to the label element
+                
                 const lowerBoundLabel = document.getElementById('lower-bound-label');
                 const upperBoundLabel = document.getElementById('upper-bound-label');
 
-                // Update the label
+                
                 lowerBoundLabel.innerHTML = lowerBound;
                 upperBoundLabel.innerHTML = upperBound;
             });
