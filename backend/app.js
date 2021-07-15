b460cf5f91b8d9d1848b4318f4e5db6265683e4a
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-15 15:16:39 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { nanoid } = require('nanoid');
+const createHttpError = require('http-errors');
 
 const MagicNumberGame = require('../logic/MagicNumberGame');
 
@@ -20,11 +21,7 @@ app.post('/', function (req, res) {
     });
 });
 
-app.listen(8000, function () {
-    console.log('Magic Number Game app listening on Port 8000');
-});
-
-app.put('/:sessionId', function (req, res) {
+app.put('/:sessionId', function (req, res, next) {
 
     const sessionId = req.params.sessionId;
 
@@ -33,22 +30,19 @@ app.put('/:sessionId', function (req, res) {
     const attemptNumber = Number(attempt);
 
     if (Number.isNaN(attemptNumber)) {
-
-        return res.status(400).json({
-            error: `Attempt (${attempt}) is Not a Number!`,
-        });
+        return next(createHttpError(400, `Attempt (${attempt}) is Not an Number!`)); 
     }
 
     if (!Number.isInteger(attemptNumber)) {
-        return res.status(400).json({
-            error: `Attempt (${attempt}) is Not an Integer!`,
-        });
+        return next(createHttpError(400, `Attempt (${attempt}) is Not an Integer!`));
     }
 
     if (attemptNumber < 0) {
-        return res.status(400).json({
-            error: `Attempt (${attempt}) is Not a Positive Integer!`,
-        });
+        return next(createHttpError(400, `Attempt (${attempt}) is Not a Positive Integer!`));
+    }
+
+    if (!sessions[sessionId]) {
+        return next(createHttpError(400, `Attempt (${sessionId}) not found!`));
     }
 
     const magicNumberGame = sessions[sessionId];
@@ -56,4 +50,21 @@ app.put('/:sessionId', function (req, res) {
     const progress = magicNumberGame.guess(attempt);
 
     return res.json(progress);
+});
+
+app.use(function (req, res, next) {
+    return next(createHttpError(404, `Not found ${req.method} ${req.originalUrl}`));
+});
+
+app.use(function (err, req, res, next) {
+    const status = err.status || 500;
+    const message = err.message || 'Unknown Error';
+    console.log(err);
+    return res.status(status).json({
+        error: message,
+    });
+});
+
+app.listen(8000, function () {
+    console.log('Magic Number Game app listening on Port 8000');
 });
