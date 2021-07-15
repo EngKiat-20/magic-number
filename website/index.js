39594ec0f9644cf7b0a33ff58ea29e08d73a50f6
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-07-15 14:51:56 +0800

    .

--- website/index.js
+++ website/index.js
@@ -1,9 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    window.alert('DOM fully loaded and parsed');
     var createSessionButton = document.getElementById("create-session-button");
+    var submitGuessButton = document.getElementById("submit-guess-button")
 
     createSessionButton.addEventListener("click", (event) => {
-        window.alert('Button pressed');
         fetch('http://localhost:8000/', {method: 'POST'})
         .then(function (response) {
             return response.json();
@@ -14,4 +13,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
             sessionIdField.innerHTML = sessionId;
         })
     });
+
+    submitGuessButton.addEventListener("click", (event) => {
+        window.alert("Submit button clicked")
+
+        const sessionIdInput = document.getElementById('session-id-input');
+        const attemptInput = document.getElementById('attempt-input');
+
+        const sessionId = sessionIdInput.value;
+        const attempt = attemptInput.value;
+
+        const url = `http://localhost:8000/${sessionId}?attempt=${attempt}`;
+
+        fetch(url, { method: 'PUT' })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                // Extract the lower and upper bound
+                const lowerBound = json[0];
+                const upperBound = json[1];
+
+                // Get the reference to the label element
+                const lowerBoundLabel = document.getElementById('lower-bound-label');
+                const upperBoundLabel = document.getElementById('upper-bound-label');
+
+                // Update the label
+                lowerBoundLabel.innerHTML = lowerBound;
+                upperBoundLabel.innerHTML = upperBound;
+            });
+    })
 });
